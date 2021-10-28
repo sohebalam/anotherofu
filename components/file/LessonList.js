00bf5b828efb5202e1ec89b70a9214a93f3e739d
soheb alam <sohebalam@hotmail.com>
2021-10-28 16:07:24 +0100

    lessonreorder

--- components/file/LessonList.js
+++ components/file/LessonList.js
@@ -1,7 +1,24 @@
-import { useEffect } from "react"
+import React, { useEffect, useState } from "react"
+import {
+  SortableElement,
+  SortableContainer,
+  arrayMove,
+} from "react-sortable-hoc"
 import axios from "axios"
+// import arrayMove from "array-move"
 
-const LesssonList = ({ slug }) => {
+const Item = SortableElement(({ value }) => <li>{value}</li>)
+
+const ItemContainer = SortableContainer(({ items }) => (
+  <ul>
+    {items.map((item, index) => (
+      <Item key={`item-${item.name}`} index={index} value={item.display} />
+    ))}
+  </ul>
+))
+
+function App({ slug }) {
+  const [extLessons, setExtLessons] = useState([])
   useEffect(() => {
     getlessons()
   }, [])
@@ -14,17 +31,52 @@ const LesssonList = ({ slug }) => {
       // files.concat(videos)
 
       const lessons = [...files, ...videos]
-      console.log(lessons)
+      setExtLessons(lessons)
     } catch (error) {
       console.log(error)
     }
   }
 
+  console.log(...extLessons)
+
+  const initalItems = extLessons.map((item) => ({
+    name: item.title,
+    display: item.title,
+    selected: false,
+  }))
+
+  console.log(initalItems)
+
+  useEffect(() => {
+    setItems(initalItems)
+  }, [setItems])
+
+  const [items, setItems] = useState(initalItems)
+
+  function toggleItemState(item) {
+    const updatedItems = items.map((currentItem) => ({
+      ...currentItem,
+      selected:
+        currentItem.name === item
+          ? !currentItem.selected
+          : currentItem.selected,
+    }))
+
+    setItems(updatedItems)
+  }
+
+  function onSortEnd({ oldIndex, newIndex }) {
+    const updatedItems = arrayMove(items, oldIndex, newIndex)
+    setItems(updatedItems)
+  }
+  console.log(items)
   return (
-    <div>
-      <h1>LesssonList </h1>
+    <div className="App">
+      <h2>Start editing to see some magic happen!</h2>
+
+      <ItemContainer items={items} onSortEnd={onSortEnd} />
     </div>
   )
 }
 
-export default LesssonList
+export default App
